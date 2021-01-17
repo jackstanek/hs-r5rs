b17aab9a4df172c0e28fe37b4e2b0dc9241c8706
Jack Stanek <stanekjack@gmail.com>
2021-01-16 19:49:11 -0600

    Refactor parsing code, add string literals

--- src-exe/Main.hs
+++ src-exe/Main.hs
@@ -1,86 +1,8 @@
 module Main where
 
-import Control.Applicative
-import Data.Either (fromRight)
+import Parse (parseExpr)
 
-import qualified Text.Parsec as P
-import Text.Parsec.String (Parser)
 import System.Console.Haskeline
-
-data AST = IntegerExpr Integer
-                | BooleanExpr Bool
-                | StringExpr String
-                | SymbolExpr String
-                | Empty -- i.e. the empty list
-                | SExpr AST AST
-  deriving (Eq, Show)
-
-commentP = P.skipMany $ do
-  P.char ';'
-  P.manyTill P.anyChar P.newline
-  return ()
-
-spaces1P = P.skipMany1 P.space
-
-ignored = spaces1P <|> commentP
-
-lexeme :: Parser a -> Parser a
-lexeme p = ignored >> p
-
--- Parse boolean literals ("#t" and "#f")
-boolP :: Parser AST
-boolP = lexeme $ do
-  P.char '#'
-  value <- P.oneOf "tf"
-  return $ BooleanExpr (value == 't')
-
-intP :: Parser AST
-intP = lexeme $ do
-  sign <- P.optionMaybe $ P.oneOf "+-"
-  value <- P.many1 P.digit
-  return $ IntegerExpr $ multiplier sign * (read value)
-    where multiplier sign = case sign of
-                              Just '-' -> -1
-                              otherwise -> 1
-
--- Parse symbols (referred to as identifiers in the spec)
-symbolP :: Parser AST
-symbolP = lexeme $ peculiarP <|> do
-  initial <- initialP
-  subsequent <- P.many $ P.choice [initialP, P.digit, P.oneOf "+-.@"]
-  return $ SymbolExpr $ initial:subsequent
-    where initialP = P.oneOf "!$%&*/:<=>?^_~" <|> P.letter
-          peculiarP = SymbolExpr <$> P.choice [P.string "+", P.string "-", P.string "..."]
-
--- helper function to create lists
-consify :: AST -> [AST] -> AST
-consify last [] = last
-consify last (x:xs) = SExpr x $ consify last xs
-buildList = consify Empty
-
--- Simple parsers for parentheses
-lparen = lexeme $ P.char '('
-rparen = lexeme $ P.char ')'
-
--- Parser for simple list notation
-listP :: Parser AST
-listP = do
-  lparen
-  exprs <- P.many exprP
-  rparen
-  return $ buildList $ exprs
-
--- Parser for pairs (e.g. "(1 . 2)")
-pairP = do
-  lparen
-  frontExprs <- P.many1 exprP
-  lexeme $ P.char '.'
-  lastExpr <- exprP
-  rparen
-  return $ consify lastExpr frontExprs
-
-exprP :: Parser AST
-exprP = P.choice $ map P.try [intP, symbolP, boolP, listP, pairP]
 
 main :: IO ()
 main = runInputT defaultSettings newLoop
@@ -90,8 +12,10 @@ main = runInputT defaultSettings newLoop
           case minput of
             Nothing -> return ()
             Just input -> let inputSoFar = prevInput ++ input in
-                          case P.parse exprP "stdin" inputSoFar of
-                            Right result -> do outputStrLn $ show $ P.parse exprP "stdin" inputSoFar
-                                               newLoop
-                            Left _ -> loop inputSoFar
+                          case (parseExpr "stdin" inputSoFar, input) of
+                            (Right result, _) -> do outputStrLn $ show result
+                                                    newLoop
+                            (Left err, "") -> do outputStrLn $ show err
+                                                 newLoop
+                            (Left _, _) -> loop $ inputSoFar ++ "\n"
         newLoop = loop ""
---
+++ src-exe/Parse.hs
@@ -0,0 +1,89 @@
+module Parse (AST, parseExpr) where
+
+import Control.Applicative
+
+import qualified Text.Parsec as P
+import Text.Parsec.String (Parser)
+
+data AST = IntegerExpr Integer
+                | BooleanExpr Bool
+                | StringExpr String
+                | SymbolExpr String
+                | Empty -- i.e. the empty list
+                | SExpr AST AST
+  deriving (Eq, Show)
+
+lexeme :: Parser a -> Parser a
+lexeme p = ignorable *> p
+  where ignorable = P.spaces <|> comments
+        comments = do P.string ";"
+                      P.many $ P.noneOf "\n"
+                      P.newline
+                      return ()
+
+-- Parse boolean literals ("#t" and "#f")
+boolP :: Parser AST
+boolP = lexeme $ do
+  P.char '#'
+  value <- P.oneOf "tf"
+  return $ BooleanExpr (value == 't')
+
+intP :: Parser AST
+intP = lexeme $ do
+  sign <- P.optionMaybe $ P.oneOf "+-"
+  value <- P.many1 P.digit
+  return $ IntegerExpr $ multiplier sign * (read value)
+    where multiplier sign = case sign of
+                              Just '-' -> -1
+                              otherwise -> 1
+
+-- Parse string literals
+stringP :: Parser AST
+stringP = lexeme $ do
+  P.char '\"'
+  contents <- P.many $ P.noneOf "\""
+  P.char '\"'
+  return $ StringExpr $ contents
+
+-- Parse symbols (referred to as identifiers in the spec)
+symbolP :: Parser AST
+symbolP = lexeme $ peculiarP <|> do
+  initial <- initialP
+  subsequent <- P.many $ P.choice [initialP, P.digit, P.oneOf "+-.@"]
+  return $ SymbolExpr $ initial:subsequent
+    where initialP = P.oneOf "!$%&*/:<=>?^_~" <|> P.letter
+          peculiarP = SymbolExpr <$> P.choice [P.string "+", P.string "-", P.string "..."]
+
+-- helper function to create lists
+consify :: AST -> [AST] -> AST
+consify last [] = last
+consify last (x:xs) = SExpr x $ consify last xs
+buildList = consify Empty
+
+-- Simple parsers for parentheses
+lparen = lexeme $ P.char '('
+rparen = lexeme $ P.char ')'
+
+-- Parser for simple list notation
+listP :: Parser AST
+listP = do
+  lparen
+  exprs <- P.many exprP
+  rparen
+  return $ buildList $ exprs
+
+-- Parser for pairs (e.g. "(1 . 2)")
+pairP = do
+  lparen
+  frontExprs <- P.many1 exprP
+  lexeme $ P.char '.'
+  lastExpr <- exprP
+  rparen
+  return $ consify lastExpr frontExprs
+
+exprP :: Parser AST
+exprP = P.choice $ map P.try [intP, stringP, symbolP, boolP, listP, pairP]
+
+programP = P.many1 $ lexeme $ exprP
+
+parseExpr = P.parse exprP
